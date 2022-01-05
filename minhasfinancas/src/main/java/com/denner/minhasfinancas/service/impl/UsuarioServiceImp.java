e50f157dc8c7f1473878cd5e8bf998e99d7dc81d
Denner Ramiro Ribeiro <dennerramiror@gmail.com>
2022-01-05 14:29:00 -0300

    Validar email, e Exception regra de negocio

--- minhasfinancas/src/main/java/com/denner/minhasfinancas/service/impl/UsuarioServiceImp.java
+++ minhasfinancas/src/main/java/com/denner/minhasfinancas/service/impl/UsuarioServiceImp.java
@@ -1,15 +1,20 @@
 package com.denner.minhasfinancas.service.impl;
 
+import com.denner.minhasfinancas.exception.RegraNegocioException;
 import com.denner.minhasfinancas.model.entity.Usuario;
 import com.denner.minhasfinancas.model.repository.UsuarioRepository;
 import com.denner.minhasfinancas.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UsuarioServiceImp implements UsuarioService {
 
     private UsuarioRepository repository;
 
-
+    @Autowired
     public UsuarioServiceImp(UsuarioRepository repository) {
+        super();
         this.repository = repository;
     }
 
@@ -25,6 +30,9 @@ public class UsuarioServiceImp implements UsuarioService {
 
     @Override
     public void validarEmail(String email) {
-
+        boolean existe = repository.existsByEmail(email);
+        if(existe){
+            throw new RegraNegocioException("Já existe um usuário cadastrado ocm este email");
+        }
     }
 }
