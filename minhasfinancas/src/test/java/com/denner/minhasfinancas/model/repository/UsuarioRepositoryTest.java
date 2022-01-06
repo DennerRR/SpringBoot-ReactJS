1d1550d32d6669f2a7f6d67708054381a3f51afe
Denner Ramiro Ribeiro <dennerramiror@gmail.com>
2022-01-06 12:43:37 -0300

    adicionando mocks no usuarioServiceTest

--- minhasfinancas/src/test/java/com/denner/minhasfinancas/model/repository/UsuarioRepositoryTest.java
+++ minhasfinancas/src/test/java/com/denner/minhasfinancas/model/repository/UsuarioRepositoryTest.java
@@ -7,9 +7,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
 
 
 @RunWith(SpringRunner.class)
@@ -21,13 +23,15 @@ public class UsuarioRepositoryTest {
     @Autowired
     UsuarioRepository repository;
 
+    @Autowired
+    TestEntityManager entityManager;
 
     @Test
     public void deveVerificarAExistenciaDeUmEmail(){
 
         //cenário
-        Usuario usuario = Usuario.builder().nome("usuario").email("usuario@email.com").build();
-        repository.save(usuario);
+        Usuario usuario = criarUsuario();
+        entityManager.persist(usuario);
 
         //ação/ execução
         boolean result = repository.existsByEmail("usuario@email.com");
@@ -39,13 +43,44 @@ public class UsuarioRepositoryTest {
 
     @Test
     public void deveRetornarFalsoQuandoNaoHouverUsuarioCadastradoComEmail(){
-        // cenário
-        repository.deleteAll();
 
         //ação / execução
         boolean result = repository.existsByEmail("usuario@email.com");
 
         //verificação
         Assertions.assertThat(result).isFalse();
+    }
+    @Test
+    public void devePersistirUmUsuarioNaBaseDeDados(){
+        //cenário
+        Usuario usuario = criarUsuario();
+        // ação
+        Usuario usuarioSalvo = repository.save(usuario);
+        //verificação
+        Assertions.assertThat(usuarioSalvo.getId()).isNotNull();
+
+    }
+
+    @Test
+    public void deveBuscarUmUsuarioPorEmail(){
+        Usuario usuario = criarUsuario();
+        entityManager.persist(usuario);
+
+        Optional<Usuario> result = repository.findByEmail("usuario@email.com");
+
+        Assertions.assertThat(result.isPresent()).isTrue();
+
+    }
+
+    @Test
+    public void deveRetornarVazioAoBuscarUmUsuarioPorEmailQuandoNaoExisteNaBase(){
+
+        Optional<Usuario> result = repository.findByEmail("usuario@email.com");
+
+        Assertions.assertThat(result.isPresent()).isFalse();
+
+    }
+    public static Usuario criarUsuario(){
+        return Usuario.builder().nome("usuario").email("usuario@email.com").senha("senha").build();
     }
 }
--- minhasfinancas/src/test/java/com/denner/minhasfinancas/model/service/UsuarioServiceTest.java
+++ minhasfinancas/src/test/java/com/denner/minhasfinancas/model/service/UsuarioServiceTest.java
@@ -2,41 +2,42 @@ package com.denner.minhasfinancas.model.service;
 
 import com.denner.minhasfinancas.exception.RegraNegocioException;
 import com.denner.minhasfinancas.model.repository.UsuarioRepository;
-import com.denner.minhasfinancas.model.entity.Usuario;
 import com.denner.minhasfinancas.service.UsuarioService;
+import com.denner.minhasfinancas.service.impl.UsuarioServiceImp;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
 @RunWith(SpringRunner.class)
 @ActiveProfiles("teste")
 public class UsuarioServiceTest {
 
-    @Autowired
     UsuarioService service;
-
-    @Autowired
     UsuarioRepository repository;
 
+    @Before
+    public void setUp(){
+        repository = Mockito.mock(UsuarioRepository.class);
+        service = new UsuarioServiceImp(repository);
+    }
     @Test(expected = Test.None.class)
     public void deveValidarEmail(){
         // cenário
-        repository.deleteAll();
+        Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(false);
+
         //ação/execução
         service.validarEmail("email@email.com");
-
 
     }
 
     @Test(expected = RegraNegocioException.class)
     public void deveLancarErroAoValidarEmailQuandoExistirEmailCadastrado(){
         //cenário
-        Usuario usuario = Usuario.builder().nome("usuario").email("email@email.com").build();
-        repository.save(usuario);
+        Mockito.when(repository.existsByEmail(Mockito.anyString())).thenReturn(true);
+
         //ação
         service.validarEmail("email@email.com");
     }
