300605660e4e5fea89a1b689883721a10e574d4c
Denner Ramiro Ribeiro <dennerramiror@gmail.com>
2022-01-07 14:50:44 -0300

    swagger config e autenticar metodo

--- minhasfinancas/src/main/java/com/denner/minhasfinancas/api/resource/UsuarioResource.java
+++ minhasfinancas/src/main/java/com/denner/minhasfinancas/api/resource/UsuarioResource.java
@@ -1,18 +1,22 @@
 package com.denner.minhasfinancas.api.resource;
 
 import com.denner.minhasfinancas.api.DTO.UsuarioDTO;
+import com.denner.minhasfinancas.exception.ErroAutenticacao;
 import com.denner.minhasfinancas.exception.RegraNegocioException;
 import com.denner.minhasfinancas.model.entity.Usuario;
 import com.denner.minhasfinancas.service.UsuarioService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/usuarios")
+@Api(value = "API Rest Usuarios")
+
+//Liberar todos domínios para acessar essa API
+@CrossOrigin(origins = "*")
 public class UsuarioResource {
 
 
@@ -22,7 +26,8 @@ public class UsuarioResource {
         this.service = service;
 
     }
-    @PostMapping
+    @PostMapping("/salvar")
+    @ApiOperation(value = "Salvar um usuario")
     public ResponseEntity salvar(@RequestBody UsuarioDTO dto){
         Usuario usuario = Usuario.builder().nome(dto.getNome()).email(dto.getEmail()).senha(dto.getSenha()).build();
 
@@ -33,6 +38,17 @@ public class UsuarioResource {
             return ResponseEntity.badRequest().body(e.getMessage());
 
 
+        }
+
+    }
+    @PostMapping("/autenticar")
+    @ApiOperation(value = "Autenticar usuario")
+    public ResponseEntity autenticar(@RequestBody UsuarioDTO dto){
+        try{
+            Usuario usuarioAutenticado = service.autenticar(dto.getEmail(), dto.getSenha());
+            return ResponseEntity.ok(usuarioAutenticado);
+        }catch(ErroAutenticacao e){
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
 
     }
