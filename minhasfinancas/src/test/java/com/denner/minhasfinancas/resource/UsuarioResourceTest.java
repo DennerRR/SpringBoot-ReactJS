f2c28cc984b7c765060cb05cf821a0ea495a378a
Denner Ramiro Ribeiro <dennerramiror@gmail.com>
2022-01-17 12:29:12 -0300

    Controller TEste autenticar

---
+++ minhasfinancas/src/test/java/com/denner/minhasfinancas/resource/UsuarioResourceTest.java
@@ -0,0 +1,70 @@
+package com.denner.minhasfinancas.resource;
+
+import com.denner.minhasfinancas.api.DTO.UsuarioDTO;
+import com.denner.minhasfinancas.api.resource.UsuarioResource;
+import com.denner.minhasfinancas.model.entity.Usuario;
+import com.denner.minhasfinancas.service.LancamentoService;
+import com.denner.minhasfinancas.service.UsuarioService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@WebMvcTest(controllers = UsuarioResource.class )
+@AutoConfigureMockMvc
+public class UsuarioResourceTest {
+
+    static final String API = "/api/usuarios";
+    static final MediaType JSON = MediaType.APPLICATION_JSON;
+
+    @Autowired
+    MockMvc mvc;
+
+    @MockBean
+    UsuarioService service;
+
+    @MockBean
+    LancamentoService lancamentoService;
+
+    @Test
+    public void deveAutenticarUmUsuario() throws Exception{
+        // cenário
+        String email = "usuario@email.com";
+        String senha = "123";
+
+        UsuarioDTO dto = UsuarioDTO.builder().email(email).senha(senha).build();
+        Usuario usuario = Usuario.builder().id(1l).email(email).senha(senha).build();
+
+        Mockito.when(service.autenticar(email,senha)).thenReturn(usuario);
+
+        String json = new ObjectMapper().writeValueAsString(dto);
+
+        // execução e verificação
+
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders
+                .post(API.concat("/autenticar"))
+                .accept(JSON)
+                .contentType(JSON)
+                .content(json);
+
+        mvc.perform(request)
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("id").value(usuario.getId()))
+                .andExpect(MockMvcResultMatchers.jsonPath("nome").value(usuario.getNome()))
+                .andExpect(MockMvcResultMatchers.jsonPath("email").value(usuario.getEmail()));
+    }
+
+}
