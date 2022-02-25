268765bab26cf29ac3573d840a1d17309764e7a6
Denner <dennerramiror@gmail.com>
2022-02-24 23:35:17 -0300

    Adicionando o CORS

--- minhasfinancas/src/main/java/com/denner/minhasfinancas/api/DTO/UsuarioDTO.java
+++ minhasfinancas/src/main/java/com/denner/minhasfinancas/api/DTO/UsuarioDTO.java
@@ -1,12 +1,12 @@
 package com.denner.minhasfinancas.api.DTO;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UsuarioDTO {
 
     private String email;
